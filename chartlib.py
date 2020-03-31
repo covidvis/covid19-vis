11690e7df85feea82cf9695eea4cec07fc3d7bea
Stephen Macke <smacke@illinois.edu>
2020-03-31 08:34:04 -0700

    use DotDict instead of Bunch

--- chartlib.py
+++ chartlib.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Union
 
-from bunch import Bunch
 import altair as alt
 import numpy as np
 import pandas as pd
@@ -41,7 +40,44 @@ class DaysSinceNumReached(StartCriterion):
         return df
 
 
-class ChartSpec(Bunch):
+# ref: https://gist.github.com/golobor/397b5099d42da476a4e6
+class DotDict(dict):
+    """A dict with dot access and autocompletion.
+
+    The idea and most of the code was taken from
+    http://stackoverflow.com/a/23689767,
+    http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named/
+    http://stackoverflow.com/questions/2390827/how-to-properly-subclass-dict-and-override-get-set
+    """
+
+    def __init__(self, *a, **kw):
+        dict.__init__(self)
+        self.update(*a, **kw)
+        self.__dict__ = self
+
+    def __setattr__(self, key, value):
+        if key in dict.__dict__:
+            raise AttributeError('This key is reserved for the dict methods.')
+        dict.__setattr__(self, key, value)
+
+    def __setitem__(self, key, value):
+        if key in dict.__dict__:
+            raise AttributeError('This key is reserved for the dict methods.')
+        dict.__setitem__(self, key, value)
+
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).items():
+            self[k] = v
+
+    def __getstate__(self):
+        return self
+
+    def __setstate__(self, state):
+        self.update(state)
+        self.__dict__ = self
+
+
+class ChartSpec(DotDict):
     X = 'x'
     Y = 'y'
 
